4ee000c977adaee982d673b77396422457dfbf2a
mrwise <sahinmustafaenes1@gmail.com>
2022-05-07 19:22:31 -0600

    @Scope Annotation is added

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/MyApp.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+
+        System.out.println(cs1 == cs2);
+
+    }
+}
--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -4,9 +4,13 @@ import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository;
