33baaafde262f4fc19c60925dd13dae3d1ebbc40
mrwise <sahinmustafaenes1@gmail.com>
2022-05-08 00:56:52 -0600

    SpringBoot Module Added

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/Spring06SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
+    }
+
+}
