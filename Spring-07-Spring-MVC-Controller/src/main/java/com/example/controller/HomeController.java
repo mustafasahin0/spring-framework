35a393c003b5aabf45238a3cfa3b8b546fd48482
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-06-09 16:39:28 -0600

    Added Notes to controller class

--- Spring-07-Spring-MVC-Controller/src/main/java/com/example/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/example/controller/HomeController.java
@@ -3,16 +3,21 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Annotate the class with the @Controller stereotype annotation
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home") // Use @RequestMapping annotation to associate the action with an HTTP request path
     public String home(){
-        return "home.html";
+        return "home.html"; // Return the HTML document name that contains the details we want the browser to display
     }
 
     @RequestMapping("/welcome")
     public String welcome(){
+        return "welcome.html";
+    }
+
+    @RequestMapping
+    public String welcome1(){
         return "welcome.html";
     }
 }
