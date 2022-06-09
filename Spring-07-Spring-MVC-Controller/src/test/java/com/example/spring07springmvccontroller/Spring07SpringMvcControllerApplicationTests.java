de1718511a2e37a7d57bef01e21a557d406010ab
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-06-09 16:23:32 -0600

    Added New Module Spring MVC

---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/example/spring07springmvccontroller/Spring07SpringMvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring07springmvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
