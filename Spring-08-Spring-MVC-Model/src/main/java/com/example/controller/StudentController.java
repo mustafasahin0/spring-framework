a122dcca98936ac3e791c01236deb107af0471b5
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-06-10 19:50:59 -0600

    Added new examples to Module

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -4,15 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
     @RequestMapping("/welcome")
-    public String homePage(Model model){
+    public String homePage(Model model) {
 
         // Model Methods
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        Random random = new Random();
+        int studentId = random.nextInt();
+        model.addAttribute("studentId", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
 
         return "/student/welcome";
     }
