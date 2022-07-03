f382d770c0ff73c7a06508b547f7726123c35de9
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-03 15:01:44 -0600

    Spring Thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/bootstrap/DataGenerator.java
@@ -0,0 +1,21 @@
+package com.example.bootstrap;
+
+import com.example.model.Student;
+import com.github.javafaker.Faker;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<Student> createStudent() {
+        List<Student> students = Arrays.asList(
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 50), new Faker().address().state()),
+                new Student(new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20, 50), new Faker().address().state())
+        );
+        return students;
+    }
+}
