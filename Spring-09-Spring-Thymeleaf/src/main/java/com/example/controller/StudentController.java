7ad30f40ffcfc6b4c44f80819ed23cc9d4efd015
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-04 12:19:31 -0600

    added register form

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -3,21 +3,25 @@ package com.example.controller;
 import com.example.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+   // @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("students", DataGenerator.createStudent());
 
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    //@RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String info(){
 
         return "student/welcome";
