832eb95c587d00fb9b206f19ceea36e91d2ff99a
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-04 13:24:03 -0600

    added confirmation page

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/MentorController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.example.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -22,5 +24,14 @@ public class MentorController {
         model.addAttribute("batchList", batchList);
 
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor") Mentor mentor) {
+
+        System.out.println(mentor);
+
+      //  return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 }
