a1ac6f33360e18b51999df8ef7d9c081b2f7eff3
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-03 19:03:41 -0600

    Added Static Files

--- Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
