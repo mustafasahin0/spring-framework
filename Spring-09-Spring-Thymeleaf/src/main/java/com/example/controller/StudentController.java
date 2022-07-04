8e50d2f6b9c9dcf9f10c6c109526a870afee8313
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-03 18:36:20 -0600

    Added Concatenation and Link

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    @RequestMapping("/welcome")
+    public String info(){
+
+        return "student/welcome";
+    }
+}
