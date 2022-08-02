546386aabe4cb5e8902007a8043c71a6c117c37f
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-08-02 11:13:03 -0600

    ORM Part-1

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
