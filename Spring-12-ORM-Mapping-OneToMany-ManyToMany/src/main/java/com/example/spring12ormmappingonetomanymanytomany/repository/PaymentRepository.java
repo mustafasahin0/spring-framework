0d136304b6e1094c44a3dcc8e7793678aca5d310
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-08-15 17:41:09 -0500

    Cinema Lab

--- Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/example/spring12ormmappingonetomanymanytomany/repository/PaymentRepository.java
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/example/spring12ormmappingonetomanymanytomany/repository/PaymentRepository.java
@@ -4,4 +4,10 @@ import com.example.spring12ormmappingonetomanymanytomany.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    // method --> sql  --> derive query --> findById(int id)
+
+    // sql - @Query
+
+    // JPQL - @Query
 }
