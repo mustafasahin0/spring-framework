04302e2e7cbb70d7bb201cff51c6a5bbcbbd10fc
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-09-03 22:31:50 -0600

    Cinema Query Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/CinemaRepository.java
@@ -6,21 +6,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to get cinema with a specific name
-    Cinema findByName(String name);
+    Optional<Cinema> findByName(String name);
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-    List<Cinema> findTop3BySponsoredName(String sponsoredName);
+    List<Cinema> findTop3BySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
-    // List<Cinema> findCinemasByLocationWher()
+     List<Cinema> listAllByLocationCountry(String country)
+
     //Write a derived query to list all cinemas with a specific name or sponsored name
-    List<Cinema> findCinemaByNameContainsOrSponsoredNameContains(String name, String sponsoredName);
+    List<Cinema> findAllByNameOrSponsoredName(String name, String sponsoredName);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
