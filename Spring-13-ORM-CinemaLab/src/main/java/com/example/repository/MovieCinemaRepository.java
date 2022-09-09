ecf1a1986672ea82f376fcbae386b808ff16a726
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-09-08 19:29:36 -0600

    Cinema Query Lab

--- Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/repository/MovieCinemaRepository.java
@@ -3,36 +3,51 @@ package com.example.repository;
 import com.example.entity.Cinema;
 import com.example.entity.MovieCinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read movie cinema with id
-    MovieCinema getMovieCinemaByCinemaId(Long id);
+    @Override
+    Optional<MovieCinema> findById(Long id);
 
     //Write a derived query to count all movie cinemas with a specific cinema id
-    List<MovieCinema> getMovieCinemasByCinemaId(Long id);
+    Integer countAllByCinemaId(Long cinemaId);
+
     //Write a derived query to count all movie cinemas with a specific movie id
-    List<List> findMovieCinemasByMovieEquals(Long id);
+    List<List> countAllByMovieId(Long movieId);
+
     //Write a derived query to list all movie cinemas with higher than a specific date
+    List<MovieCinema> findAllByDateTimeAfter(LocalDateTime dateTime);
 
     //Write a derived query to find the top 3 expensive movies
+    List<MovieCinema> findFirst3ByOrderByMoviePriceDesc();
 
     //Write a derived query to list all movie cinemas that contain a specific movie name
+    List<MovieCinema> findAllByMovieNameContaining(String name);
 
     //Write a derived query to list all movie cinemas in a specific location
-
+    List<MovieCinema> findAllByCinemaLocationName(String name);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movie cinemas with higher than a specific date
+    @Query("SELECT mc FROM MovieCinema mc WHERE mc.dateTime > ?1")
+    List<MovieCinema> fetchAllWithHigherThanSpecificDate(@Param("dateTime") LocalDateTime dateTime);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count all movie cinemas by cinema id
-
+    @Query(value = "SELECT count(*) FROM movie_cinema WHERE cinema_id = ?1", nativeQuery = true)
+    Integer countByCinemaId();
     //Write a native query that returns all movie cinemas by location name
+    @Query(value = "SELECT * FROM movie_cinema mc JOIN cinema c ON c.id = mc.cinema_id JOIN location l ON l.id = c.location_id WHERE l.name = ?1")
+    List<MovieCinema> retrieveAllByLocationName(@Param("name") String name);
 }
