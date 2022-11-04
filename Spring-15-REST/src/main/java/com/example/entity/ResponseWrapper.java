00a3f9a866a722c87acb149663616a2fe619b051
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-11-04 16:25:44 -0600

    Rest

---
+++ Spring-15-REST/src/main/java/com/example/entity/ResponseWrapper.java
@@ -0,0 +1,32 @@
+package com.example.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ResponseWrapper {
+
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.data = data;
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+}
+
+
