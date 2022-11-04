b83dc11adf6d217f4a033385e2fc0aa68f33b2df
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-11-04 16:49:07 -0600

    Rest

--- Spring-16-Jackson/src/main/java/com/example/entity/User.java
+++ Spring-16-Jackson/src/main/java/com/example/entity/User.java
@@ -1,7 +1,9 @@
 package com.example.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,18 +13,19 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @Table(name = "user_account")
+@JsonIgnoreProperties(value = "{hibernateLazyInitializer}", ignoreUnknown = true)
 public class User extends BaseEntity {
 
     @JsonIgnore
     private String email;
 
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
     private String username;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_details_id")
     @JsonManagedReference // is the forward part of reference - the one that gets serialized normally
     private Account account;
-
 }
