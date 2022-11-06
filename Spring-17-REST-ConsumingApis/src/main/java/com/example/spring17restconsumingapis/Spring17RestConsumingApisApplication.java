47c0a032abfa2ed2533f572f5ed4857002864e1f
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-11-05 22:27:13 -0600

    Rest

---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/example/spring17restconsumingapis/Spring17RestConsumingApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring17restconsumingapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring17RestConsumingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApisApplication.class, args);
+    }
+
+}
---
+++ Spring-17-REST-ConsumingApis/src/test/java/com/example/spring17restconsumingapis/Spring17RestConsumingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring17restconsumingapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring17RestConsumingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
