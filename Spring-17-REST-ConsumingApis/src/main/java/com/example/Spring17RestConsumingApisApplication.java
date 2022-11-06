36ec13b6304934cf8cb8ccd109eadcb2d3fc68bb
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-11-06 12:08:04 -0700

    Rest Consuming

--- Spring-17-REST-ConsumingApis/src/main/java/com/example/Spring17RestConsumingApisApplication.java
+++ Spring-17-REST-ConsumingApis/src/main/java/com/example/Spring17RestConsumingApisApplication.java
@@ -2,10 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Spring17RestConsumingApisApplication {
 
     public static void main(String[] args) {
@@ -16,5 +18,4 @@ public class Spring17RestConsumingApisApplication {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
 }
---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/example/controller/Consume_FeignClient.java
@@ -0,0 +1,5 @@
+package com.example.controller;
+
+public class Consume_FeignClient {
+
+}
