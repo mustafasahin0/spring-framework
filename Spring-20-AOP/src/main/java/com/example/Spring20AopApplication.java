5bc41899ce99c16e847e31cc50ea617a5f34d851
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-03 20:19:44 -0700

    Module added

---
+++ Spring-20-AOP/src/main/java/com/example/Spring20AopApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20AopApplication.class, args);
+    }
+
+}
