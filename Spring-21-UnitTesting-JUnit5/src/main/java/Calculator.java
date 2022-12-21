3aaaa8ab1164d01d9ac9b9ce33cc4e4b6396e1d8
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-21 10:12:21 -0700

    Added First Test

--- Spring-21-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ Spring-21-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,2 +1,6 @@
 public class Calculator {
+
+    public static int add (int num1, int num2) {
+        return num1 + num2;
+    }
 }
---
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        int actual = Calculator.add(2,3);
+    }
+
+}
