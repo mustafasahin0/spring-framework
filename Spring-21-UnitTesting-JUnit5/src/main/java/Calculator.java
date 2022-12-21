f29d5b1975811ceb7657c76ca505881e50d7498d
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-21 12:51:46 -0700

    Added assertions

--- Spring-21-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ Spring-21-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,6 +1,15 @@
 public class Calculator {
 
-    public static int add (int num1, int num2) {
+    public static String operator = "add";
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int add2(int num1, int num2) {
+        if(num1 > num2){
+            throw new IllegalArgumentException();
+        }
         return num1 + num2;
     }
 }
--- Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,12 +1,55 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    void add() {
-        int actual = Calculator.add(2,3);
+    @BeforeEach
+    void setUpEach() {
+        System.out.println("BeforeEach is executed");
     }
 
+    @Test
+    void test_case1() {
+        fail("Not Implemented yet");
+    }
+
+    @Test
+    void test_case2() {
+        fail("Not Implemented yet");
+    }
+
+    @Test
+    void test_case3() {
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 2, 3});
+    }
+
+    @Test
+    void test_case4() {
+        String nullString = null;
+        String notNullString = "Example";
+        assertNull(nullString);
+        assertNotNull(notNullString);
+    }
+
+    @Test
+    void test_case5() {
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+        assertSame(c1, c2);
+        assertNotSame(c1, c3);
+    }
+
+    @Test
+    void add() {
+        int actual = Calculator.add(2, 3);
+        assertEquals(5, actual, "It is not matching with expected value.");
+    }
+
+    @Test
+    void add2() {
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(5, 2));
+    }
 }
