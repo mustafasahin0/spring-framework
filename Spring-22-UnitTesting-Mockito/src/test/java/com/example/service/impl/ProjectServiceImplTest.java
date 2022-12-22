d5700ba9f87d6da1d4cdf77dfd22dc26c9736202
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-21 20:47:54 -0700

    Added Mockito Module

---
+++ Spring-22-UnitTesting-Mockito/src/test/java/com/example/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.service.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceImplTest {
+
+
+
+}
